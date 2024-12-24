35d78606c41f86fdbcd83e5356f9efe044a93cce
Camila Gallo <camilagallo1700@gmail.com>
2024-12-24 15:41:11 -0300

    .

--- target/debug/deps/lists-1b23dd5f6bfe2af2.d
+++
@@ -1,8 +0,0 @@
-/home/cami/Desktop/en_git_hub/lists_in_rust/target/debug/deps/liblists-1b23dd5f6bfe2af2.rmeta: src/lib.rs src/first.rs
-
-/home/cami/Desktop/en_git_hub/lists_in_rust/target/debug/deps/liblists-1b23dd5f6bfe2af2.rlib: src/lib.rs src/first.rs
-
-/home/cami/Desktop/en_git_hub/lists_in_rust/target/debug/deps/lists-1b23dd5f6bfe2af2.d: src/lib.rs src/first.rs
-
-src/lib.rs:
-src/first.rs:
